1e3ed2a8c9505cd4e6e32fd17919572c9218ddda
JockaliaS <144474737+JockaliaS@users.noreply.github.com>
2025-09-25 10:33:26 -0400

    Corriger les erreurs WebSocket HMR

--- vite.config.js
+++ vite.config.js
@@ -217,6 +217,9 @@ export default defineConfig({
 			'Cross-Origin-Embedder-Policy': 'credentialless',
 		},
 		allowedHosts: true,
+		hmr: {
+			clientPort: 5173
+		}
 	},
 	resolve: {
 		extensions: ['.jsx', '.js', '.tsx', '.ts', '.json', ],
